81e975821803bd9ed9f7c2cc7fb445a38a741994
Dan <danielzuzevich@gmail.com>
2021-08-10 14:19:03 -0400

    removed body parser, built into express now

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 /* eslint-disable no-console */
 const express = require('express')
 const http = require('http')
-const bodyParser = require('body-parser')
 const app = express()
 const cors = require('cors')
 const morgan = require('morgan')
@@ -9,7 +8,7 @@ const morgan = require('morgan')
 const router = require('./router')
 
 app.use(morgan('dev'))
-app.use(bodyParser.json())
+app.use(express.json())
 app.use(cors())
 
 router(app)
